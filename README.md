3c81ce8b871e4833eba69abe7f500f12913797fb
LexShand <lex280695@gmail.com>
2024-06-16 21:10:59 +0300

    website displays information about employees from data base

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Hello, world! site  
-This project is a django website that displays 'Hello, world!'.  
-And outputs information about employees from data base to the console.
+This project is a django website that displays information about employees from data base.
--- djangoproj_hello_world_site/workers/views.py
+++ djangoproj_hello_world_site/workers/views.py
@@ -7,19 +7,24 @@ def workers_page(request):
     # new_worker = Worker(name='Пётр', second_name='Иванов', salary=50000)
     # new_worker.save()
     
-    worker_to_change = Worker.objects.get(id=4) # достаём запись из базы по аттрибуту
-    print(worker_to_change)
-    worker_to_change.second_name = 'Шаманов' # меняем фамилию в записи
-    worker_to_change.save() # сохраняем в базу
-    print(worker_to_change)
+    # worker_to_change = Worker.objects.get(id=4) # достаём запись из базы по аттрибуту
+    # print(worker_to_change)
+    # worker_to_change.second_name = 'Шаманов' # меняем фамилию в записи
+    # worker_to_change.save() # сохраняем в базу
+    # print(worker_to_change)
     # Worker.objects.filter(id=4).update(second_name='Шаманов') # способ в одну строку
 
     all_workers = Worker.objects.all() # выбираем все объекты класса Worker
-    print(all_workers)
+    # print(all_workers)
     
-    workers_filtered = Worker.objects.filter(salary=100000) # выбираем все записи класса Worker по аттрибуту
-    print(workers_filtered)
+    # workers_filtered = Worker.objects.filter(salary=100000) # выбираем все записи класса Worker по аттрибуту
+    # print(workers_filtered)
 
-    for i in all_workers:
-        print(i.second_name, i.name, i.salary, i.id) # построчно выводим все записи
-    return render(request, 'index.html')
+    # for i in all_workers:
+    #     print(i.second_name, i.name, i.salary, i.id) # построчно выводим все записи
+    
+    # context_dict = {'data': 'olololo',
+    #                 'data2': 'O_____o'}
+    context_dict = {'data': all_workers}
+
+    return render(request, 'index.html', context=context_dict)
